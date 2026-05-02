ea4f38a69fc8b379845720805b3e52a591a8a189
harrysooe-prog <harrysooe@gmail.com>
2026-05-02 11:04:56 +0200

    Update publish-wix.js

--- pages/api/publish-wix.js
+++ pages/api/publish-wix.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch('https://www.wixapis.com/blog/v3/posts', {
+    const response = await fetch('https://www.wixapis.com/blog/v3/draft-posts', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -50,7 +50,8 @@ export default async function handler(req, res) {
       })
     }
 
-res.json({ success: true, postId: data.post?.id, postUrl: data.post?.url })
+    res.json({ success: true, postId: data.post?.id, postUrl: data.post?.url })
+
   } catch (err) {
     res.status(500).json({ error: 'Publishing fehlgeschlagen: ' + err.message })
   }
